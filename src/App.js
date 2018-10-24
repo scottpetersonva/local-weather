c70cb37bb33cff5ea0b423183df9b73b3b41a7c0
Scott Peterson <scott.peterson.va@gmail.com>
2018-10-24 14:00:56 -0400

    added config keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import API_K from './config_keys.js'
 
 import Plot from 'react-plotly.js';
 import Moment from 'react-moment';
